24815dcf05850c983fc82701432a6ab9f38d1b6e
iVanKunAn <80316356+iVanKunAn@users.noreply.github.com>
2026-03-06 20:41:24 +0500

    Delete .pio/build directory

--- .pio/build/esp32-c3-devkitm-1/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-c3-devkitm-1/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/kunit/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/kunit/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
